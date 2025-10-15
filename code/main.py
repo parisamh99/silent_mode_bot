97d0fed46c72479f71b9caeeaa669eb9ac0127cc
parisamh99 <parisa.mahmoodnia@gmail.com>
2025-10-15 01:12:54 -0700

    create silent mode

---
+++ code/main.py
@@ -0,0 +1,45 @@
+import telebot
+import os
+import logging
+from telebot.types import ChatPermissions
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+
+
+#API_TOKEN = '<api_token>'
+API_TOKEN = os.environ.get("API_TOKEN")
+
+bot = telebot.TeleBot(API_TOKEN)
+
+def is_admin(message):
+  chat_id = message.chat.id
+  user_id = message.from_user.id
+  user_info = bot.get_chat_member(chat_id,user_id)
+  return user_info.status in ["creator","administrator"]
+
+
+
+@bot.message_handler(func=is_admin,commands=['restrict'])
+def restriction(message):
+    permissions = ChatPermissions(can_send_messages=False)
+    bot.set_chat_permissions(chat_id=message.chat.id,permissions=permissions)
+    bot.delete_message(message.chat.id,message.message.id)
+    bot.send_message(message.chat.id,"night mode activated")
+    logger.info("applying restriction")
+
+    
+
+@bot.message_handler(func=is_admin,commands=['unrestrict'])
+def unrestriction(message):
+    permissions = ChatPermissions(can_send_messages=True)
+    bot.set_chat_permissions(chat_id=message.chat.id,permissions=permissions)
+    bot.delete_message(message.chat.id,message.message.id)
+    bot.send_message(message.chat.id,"night mode deactivated")
+    logger.info("applying unrestriction")
+
+
+
+
+bot.infinity_polling()
